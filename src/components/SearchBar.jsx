1be92b038a7817f4bf12d5112bd80d79f8980c4c
Jake Casartelli <casartellijake@yahoo.com>
2022-11-21 18:36:19 -0500

    master commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function SearchBar(props) {
+    let [ searchTerm, setSearchTerm] = useState('')
+  return (
+    <div>
+        <form onSubmit = {(e) => props.handleSearch(e, searchTerm)}>
+            <input type="text" placeholder="Enter a search term here"
+            onChange = {(e) => setSearchTerm(e.target.value)}/>
+            <input type = "submit"/>
+        </form>
+    </div>
+  )
+}
+
+export default SearchBar
+
